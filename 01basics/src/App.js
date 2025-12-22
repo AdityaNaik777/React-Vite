0f32bbf7fad14cc42b0764ea7387fdea0ca04ade
Aditya Naik <adlearn536@gmail.com>
2025-12-22 12:26:12 +0530

    Add React basics project

---
+++ 01basics/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <h1>
+      Hello welcome chai loge? | myself aditya naik it was a nice first day with
+      chai aur code-react
+    </h1>
+  );
+}
+
+export default App;
