8819634b428cf3fa3879e8a1246b1e2620c5dd14
Aditya Naik <adlearn536@gmail.com>
2025-12-24 12:16:21 +0530

    Simple use of tailwindcss

---
+++ 07tailwindcss/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return <div className='h-full w-full bg-grey-900'>This is how it works</div>;
+}
+
+export default App;
